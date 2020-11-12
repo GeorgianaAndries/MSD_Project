f8fc9969e8236383273ff079e9bfbb0446958180
Biancabc <48654886+Biancabc@users.noreply.github.com>
2020-11-12 16:04:14 +0200

    Update UserDto.java

--- msd/demo/src/main/java/com/example/demo/dto/UserDto.java
+++ msd/demo/src/main/java/com/example/demo/dto/UserDto.java
@@ -1,44 +1,17 @@
 package com.example.demo.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
 public class UserDto {
 	private int id;
 	private String name;
 	private String password;
 	private String email;
-	 
-	public 	UserDto() {
-		
-	}
-	public UserDto(int id, String name, String password, String email) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.password = password;
-		this.email = email;
-	}
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	
 }
