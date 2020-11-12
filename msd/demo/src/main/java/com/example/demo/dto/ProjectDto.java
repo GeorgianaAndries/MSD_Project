8ebb3f80d520026c2a7e0f8460c97aa72ba01d1b
Biancabc <48654886+Biancabc@users.noreply.github.com>
2020-11-12 16:03:09 +0200

    Update ProjectDto.java

--- msd/demo/src/main/java/com/example/demo/dto/ProjectDto.java
+++ msd/demo/src/main/java/com/example/demo/dto/ProjectDto.java
@@ -1,29 +1,15 @@
 package com.example.demo.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
 public class ProjectDto {
 	private int id;
 	private String projectName;
-	
-	public ProjectDto() {
-	}
-	/*
-	public ProjectDto(int id, String projectName) {
-		super();
-		this.id = id;
-		this.projectName = projectName;
-	}*/
-	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getProjectName() {
-		return projectName;
-	}
-	public void setProjectName(String projectName) {
-		this.projectName = projectName;
-	}
-	
 }
