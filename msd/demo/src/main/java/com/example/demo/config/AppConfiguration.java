4535c2077676d19e9aad7ce8611eed7b984eb8f8
Biancabc <48654886+Biancabc@users.noreply.github.com>
2020-11-12 16:08:01 +0200

    Update and rename ModelMapperConfiguration.java to AppConfiguration.java

--- msd/demo/src/main/java/com/example/demo/config/AppConfiguration.java
+++ msd/demo/src/main/java/com/example/demo/config/AppConfiguration.java
@@ -1,12 +1,13 @@
 package com.example.demo.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class ModelMapperConfiguration {
+public class AppConfiguration {
 
     @Bean
     public ModelMapper modelMapper() {
@@ -14,5 +15,10 @@ public class ModelMapperConfiguration {
         modelMapper.getConfiguration()
                 .setMatchingStrategy(MatchingStrategies.STANDARD);
         return modelMapper;
+    }
+
+    @Bean
+    public ObjectMapper objectMapper(){
+        return new ObjectMapper();
     }
 }
