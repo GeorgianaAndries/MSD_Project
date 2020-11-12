7a5e074f50bb4c1c6e6bf7ae945d5f784797ae8d
Biancabc <48654886+Biancabc@users.noreply.github.com>
2020-11-12 16:06:39 +0200

    Update User.java

--- msd/demo/src/main/java/com/example/demo/model/User.java
+++ msd/demo/src/main/java/com/example/demo/model/User.java
@@ -7,9 +7,18 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
 @EntityListeners(AuditingEntityListener.class)
 @Entity
 @Table(name="users")
@@ -19,57 +28,15 @@ public class User {
 	private int id;
 	
 	@Column
+	@NotBlank(message = "Name is mandatory")
 	private String name;
 	
 	@Column
+	@NotBlank(message = "Email is mandatory")
 	private String email;
 	
 	@Column
 	private String password;
 
-	public User(int id, String name, String email, String password) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.email = email;
-		this.password = password;
-	}
-	
-	public User() {
-		
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
-
 }
+
